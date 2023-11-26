6a607ecb4c652d8f259ed34b572039471d111808
kareckyy <karol.jagodzinski19@gmail.com>
2023-11-26 10:45:13 +0100

    o
    pozdro

---
+++ quizapp/backend/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizapp.backend
+{
+    // internal
+    // public
+    // private
+    public class Question
+    {
+        public string Content { get; set; }
+        public int Category { get; set; }
+        public string Answer_01 { get; set; }
+        public string Answer_02 { get; set; }
+        public string Answer_03 { get; set; }
+        public string Answer_04 { get; set; }
+
+    }
+}
